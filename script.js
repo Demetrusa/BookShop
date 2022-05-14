d6fbfe2eb42db4f28fd723d2c4b3bd6f5cd08fd7
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-05-14 23:19:33 +0400

    feat: add images

--- script.js
+++ script.js
@@ -22,23 +22,36 @@ fetch("./assets/JSON.json")
             div.id = "book-wrap";
             div.className = "Book-wrap";
             document.getElementsByTagName("body")[0].appendChild(div);
-            div.innerHTML = `${apiData.title}`;
+
+            var leftDiv = document.createElement("div")
+            leftDiv.className = "Left-wrap";
+            div.appendChild(leftDiv)
+
+           
 
             var img = document.createElement("img")
             img.className = "bookImg";
             img.src = `${apiData.imageLink}`;
-            div.appendChild(img);
+            leftDiv.appendChild(img);
 
+            var rightDiv = document.createElement("div")
+            rightDiv.className = "rightDiv"
+            div.appendChild(rightDiv)
+
+            var bookTitle = document.createElement("h2")
+            bookTitle.className = "book-title";
+            rightDiv.appendChild(bookTitle)
+            bookTitle.innerHTML = `${apiData.title}`;
 
             var h3 = document.createElement("h3");
             h3.className = "h3";
-            div.appendChild(h3);
+            rightDiv.appendChild(h3);
             h3.innerHTML = `${apiData.author}`
 
             var h4 = document.createElement("h4");
             h4.className = "h4"
             h4.id = "h4"
-            div.appendChild(h4)
+            rightDiv.appendChild(h4)
             h4.innerHTML = ` ${apiData.price}` + " USD"
 
        
